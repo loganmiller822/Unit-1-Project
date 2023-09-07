ed7e49e4c15571ea244e134483ef7922f4689ce9
Logan Miller <logan.miller@southeasttech.edu>
2023-09-07 15:53:01 -0500

    Add project files.

---
+++ Unit 1 Project/Controllers/HomeController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Unit_1_Project.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
