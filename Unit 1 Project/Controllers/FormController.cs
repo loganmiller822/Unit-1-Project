ec884c839cea409bb2a9fceb330afd678e6acb46
gnarlsley <thompson.garrett7413@gmail.com>
2023-09-11 12:59:06 -0500

    Added matrix determinant functionality. Does not have input authentication

---
+++ Unit 1 Project/Controllers/FormController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Reflection;
+using Unit_1_Project.Models;
+
+namespace Unit_1_Project.Controllers
+{
+    public class FormController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult DisplayActionName(GarrettModel Model)
+        {
+            if (ModelState.IsValid)
+            { 
+                int result = (Model.Num1 * Model.Num4) - (Model.Num2 * Model.Num3); // Perform the calculation
+                ViewBag.Result = result; // Pass the result to the view
+            }
+            return View("Display"); // Return the display view
+        }
+    }
+}
--- Unit 1 Project/Models/GarrettModel.cs
+++ Unit 1 Project/Models/GarrettModel.cs
@@ -2,9 +2,9 @@
 {
     public class GarrettModel
     {
-        public string? Name { get; set; }
-        public string? Description { get; set; }
-        public string? Occupation { get; set; }
-        public int? Number { get; set; }
+        public int Num1 { get; set; }
+        public int Num2 { get; set; }
+        public int Num3 { get; set; }
+        public int Num4 { get; set; }
     }
 }
