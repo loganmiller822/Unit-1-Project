e883381b92f7a0f0119d7cdac8e17d2b1ea234a8
gnarlsley <thompson.garrett7413@gmail.com>
2023-09-11 11:13:14 -0500

    homeview

---
+++ Unit 1 Project/Models/GarrettModel.cs
@@ -0,0 +1,10 @@
+﻿namespace Unit_1_Project.Models
+{
+    public class GarrettModel
+    {
+        public string? Name { get; set; }
+        public string? Description { get; set; }
+        public string? Occupation { get; set; }
+        public int? Number { get; set; }
+    }
+}
